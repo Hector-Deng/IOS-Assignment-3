d48f1f6d59a612a56f8b619616860f9a54cb6ca5
Hector-Deng <1391812054@qq.com>
2024-04-30 11:16:46 +1000

    Initial Commit

---
+++ IOS Assignment 3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS Assignment 3
+//
+//  Created by 密码：0000 on 2024/4/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS Assignment 3/IOS_Assignment_3App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Assignment_3App.swift
+//  IOS Assignment 3
+//
+//  Created by 密码：0000 on 2024/4/30.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Assignment_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
