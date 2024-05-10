7320dcf4719160720ae40f08e4a12274915d16c6
Hector-Deng <1391812054@qq.com>
2024-05-10 18:04:57 +1000

    0

---
+++ IOS Assignment 3/FavoriteListView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteListView.swift
+//  IOS Assignment 3
+//
+//  Created by 密码：0000 on 2024/5/10.
+//
+
+import SwiftUI
+
+struct FavoriteListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteListView()
+}
